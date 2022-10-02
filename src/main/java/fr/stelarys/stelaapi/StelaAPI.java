2eb03ca80674cc04f555da70cf7dbda8cf846866
Plhume <felix@exodius.fr>
2022-10-02 16:49:33 +0200

    Initial Commit

--- src/main/java/fr/stelarys/stelaapi/StelaAPI.java
+++ src/main/java/fr/stelarys/stelaapi/StelaAPI.java
@@ -2,16 +2,18 @@ package fr.stelarys.stelaapi;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * @author Plhume
+ */
 public final class StelaAPI extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        // Do stuff
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // Do stuff
     }
 }
